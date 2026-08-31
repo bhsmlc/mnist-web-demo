7c8734aca9456c7554b5014b6350d90e0de6b9de
Ash_ioL <ashwathn.arasurajsankar@gmail.com>
2026-08-31 11:08:30 -0700

    replaced dashes in footer with dots

--- script.js
+++ script.js
@@ -30,23 +30,54 @@ async function testPredict() {
     alert(data);
     document.getElementById("ya").innerText = JSON.stringify(data);
 }
-function listenForImageUpload() {
+async function listenForImageUpload() {
     const inputTag = document.getElementById("camera-input");
     const loadingImage = document.getElementById("processing-image");
     const inputPhase = document.getElementById("input-phase");
     const processingPhase = document.getElementById("processing-phase");
     const outputPhase = document.getElementById("output-phase");
-    inputTag.addEventListener("change", (event) => {
+    inputTag.addEventListener("change", async (event) => {
         const file = inputTag.files?.[0];
         if (file) {
             console.log("File selected:", file);
             inputPhase.style.display = "none";
-            processingPhase.style.display = "block";
+            processingPhase.style.display = "flex";
+            document.getElementById("time-notice").classList.remove("show");
+            setTimeout(() => {
+                document.getElementById("time-notice").classList.add("show");
+            }, 60000); // wait 60s and then show the nootice
             loadingImage.src = URL.createObjectURL(file);
+            let formData = new FormData();
+            formData.append("image", file);
             document.getElementById("file-name").innerText = file.name;
+            const results = await fetch("https://mnist-api-74qj.onrender.com/predict-frame", {
+                method: "POST",
+                body: formData
+            });
+            const data = await results.json();
+            processingPhase.style.display = "none";
+            inputPhase.style.display = "flex";
+            outputPhase.style.display = "flex";
+            console.log(data);
+            document.getElementById("output-image").src = "data:image/jpeg;base64," + data["preprocessed-img"];
+            document.getElementById("prediction").innerText = `${parseInt(data.prediction) == -1 ? "DIGIT IS UNCLEAR" : data.prediction}`;
+            document.getElementById("score").innerText = `${parseInt(data.confidence) == -1 ? "DIGIT IS UNCLEAR" : parseFloat(data.confidence) * 100 + "%"}`;
+            let confidenceFloat = Math.min(1, Math.max(0, parseFloat(data.confidence)));
+            let red = 255 - confidenceFloat * 255;
+            let green = confidenceFloat * 255;
+            document.getElementById("score").style.color = `rgb(${red}, ${green}, 0)`;
+            document.getElementById("prediction").style.color = `rgb(${red}, ${green}, 0)`;
         }
     });
 }
+function listenForOutputClose() {
+    const outputPhase = document.getElementById("output-phase");
+    const closeButton = document.getElementById("close-icon");
+    closeButton.addEventListener("click", () => {
+        outputPhase.style.display = "none";
+    });
+}
+listenForOutputClose();
 listenForImageUpload();
 export {};
 // testPredict();
--- script.ts
+++ script.ts
@@ -48,22 +48,56 @@ async function testPredict() {
     alert(data);
     (document.getElementById("ya") as HTMLElement).innerText = JSON.stringify(data);
 }
-function listenForImageUpload() {
+
+async function listenForImageUpload() {
     const inputTag: HTMLInputElement = document.getElementById("camera-input") as HTMLInputElement;
     const loadingImage: HTMLImageElement = document.getElementById("processing-image") as HTMLImageElement;
     const inputPhase: HTMLDivElement = document.getElementById("input-phase") as HTMLDivElement;
     const processingPhase: HTMLDivElement = document.getElementById("processing-phase") as HTMLDivElement;
     const outputPhase: HTMLDivElement = document.getElementById("output-phase") as HTMLDivElement;
-    inputTag.addEventListener("change", (event: Event) => {
+    inputTag.addEventListener("change", async (event: Event) => {
         const file = inputTag.files?.[0];
         if (file) {
             console.log("File selected:", file);
             inputPhase.style.display = "none";
-            processingPhase.style.display = "block";
+            processingPhase.style.display = "flex";
+            (document.getElementById("time-notice") as HTMLElement).classList.remove("show");
+            setTimeout(() => {
+                (document.getElementById("time-notice") as HTMLElement).classList.add("show");
+            }, 60000); // wait 60s and then show the nootice
             loadingImage.src = URL.createObjectURL(file);
+            let formData: FormData = new FormData();
+            formData.append("image", file);
             (document.getElementById("file-name") as HTMLElement).innerText = file.name;
+            const results: Response = await fetch("https://mnist-api-74qj.onrender.com/predict-frame", {
+                method: "POST",
+                body: formData
+            });
+            const data: ApiResponse = await results.json();
+            processingPhase.style.display = "none";
+            inputPhase.style.display = "flex";
+            outputPhase.style.display = "flex";
+            console.log(data);
+            (document.getElementById("output-image") as HTMLImageElement).src = "data:image/jpeg;base64," + data["preprocessed-img"];
+            (document.getElementById("prediction") as HTMLElement).innerText = `${parseInt(data.prediction) == -1 ? "DIGIT IS UNCLEAR" : data.prediction}`;
+            (document.getElementById("score") as HTMLElement).innerText = `${parseInt(data.confidence) == -1 ? "DIGIT IS UNCLEAR" : parseFloat(data.confidence) * 100 + "%"}`;
+            let confidenceFloat: number = Math.min(1, Math.max(0, parseFloat(data.confidence)));
+            let red: number = 255 - confidenceFloat * 255;
+            let green: number = confidenceFloat * 255;
+            (document.getElementById("score") as HTMLElement).style.color = `rgb(${red}, ${green}, 0)`;
+            (document.getElementById("prediction") as HTMLElement).style.color = `rgb(${red}, ${green}, 0)`;
         }
     });
 }
+
+function listenForOutputClose() {
+    const outputPhase: HTMLDivElement = document.getElementById("output-phase") as HTMLDivElement;
+    const closeButton: HTMLElement = document.getElementById("close-icon") as HTMLElement;
+    closeButton.addEventListener("click", () => {
+        outputPhase.style.display = "none";
+    });
+}
+
+listenForOutputClose();
 listenForImageUpload();
 // testPredict();
