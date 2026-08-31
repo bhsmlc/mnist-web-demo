2cbb4371f3830055661acc3f9045541a587a96d3
Ash_ioL <ashwathn.arasurajsankar@gmail.com>
2026-08-31 13:05:53 -0700

    changed back to relative paths

--- script.ts
+++ script.ts
@@ -14,40 +14,40 @@ let currentPhase: string = "input";
 
 (document.getElementById("ya") as HTMLElement).innerText = greeting("world");
 
-async function testPredict() {
+// async function testPredict() {
     
-    let imgElement : HTMLImageElement = document.getElementById("image") as HTMLImageElement;
+//     let imgElement : HTMLImageElement = document.getElementById("image") as HTMLImageElement;
 
-    let response : Response = await fetch(imgElement.src);
+//     let response : Response = await fetch(imgElement.src);
 
-    if (!response.ok) {
-        alert("error");
-        throw new Error("ugh");
-    }
-    console.log(response);
-    const blob: Blob = await response.blob();
+//     if (!response.ok) {
+//         alert("error");
+//         throw new Error("ugh");
+//     }
+//     console.log(response);
+//     const blob: Blob = await response.blob();
     
-    console.log(blob);
-    let file: File = new File([blob], "test.png", { type: blob.type });
+//     console.log(blob);
+//     let file: File = new File([blob], "test.png", { type: blob.type });
 
-    let formData: FormData = new FormData();
+//     let formData: FormData = new FormData();
 
-    formData.append("image", file);
+//     formData.append("image", file);
 
-    alert("aaa");
-    const resp: Response = await fetch( "https://mnist-api-74qj.onrender.com/predict-frame", {
-        method: "POST",
-        body: formData
-    });
+//     alert("aaa");
+//     const resp: Response = await fetch( "https://mnist-api-74qj.onrender.com/predict-frame", {
+//         method: "POST",
+//         body: formData
+//     });
 
-    if (!resp.ok) {
-        throw new Error("ughhh");
-    }
-    alert("1")
-    const data: ApiResponse = await resp.json();
-    alert(data);
-    (document.getElementById("ya") as HTMLElement).innerText = JSON.stringify(data);
-}
+//     if (!resp.ok) {
+//         throw new Error("ughhh");
+//     }
+//     alert("1")
+//     const data: ApiResponse = await resp.json();
+//     alert(data);
+//     (document.getElementById("ya") as HTMLElement).innerText = JSON.stringify(data);
+// }
 
 async function listenForImageUpload() {
     const inputTag: HTMLInputElement = document.getElementById("camera-input") as HTMLInputElement;
