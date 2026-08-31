af3a0f121837c7d8d56834d80d52769ab8cacd78
Ash_ioL <ashwathn.arasurajsankar@gmail.com>
2026-08-31 11:32:06 -0700

    improved file organizationa nd used global file paths

--- script.js
+++ script.js
@@ -3,33 +3,32 @@ function greeting(name) {
 }
 let currentPhase = "input";
 // input -> processing -> output
-document.getElementById("ya").innerText = greeting("world");
-async function testPredict() {
-    let imgElement = document.getElementById("image");
-    let response = await fetch(imgElement.src);
-    if (!response.ok) {
-        alert("error");
-        throw new Error("ugh");
-    }
-    console.log(response);
-    const blob = await response.blob();
-    console.log(blob);
-    let file = new File([blob], "test.png", { type: blob.type });
-    let formData = new FormData();
-    formData.append("image", file);
-    alert("aaa");
-    const resp = await fetch("https://mnist-api-74qj.onrender.com/predict-frame", {
-        method: "POST",
-        body: formData
-    });
-    if (!resp.ok) {
-        throw new Error("ughhh");
-    }
-    alert("1");
-    const data = await resp.json();
-    alert(data);
-    document.getElementById("ya").innerText = JSON.stringify(data);
-}
+// async function testPredict() {
+//     let imgElement = document.getElementById("image");
+//     let response = await fetch(imgElement.src);
+//     if (!response.ok) {
+//         alert("error");
+//         throw new Error("ugh");
+//     }
+//     console.log(response);
+//     const blob = await response.blob();
+//     console.log(blob);
+//     let file = new File([blob], "/test.png", { type: blob.type });
+//     let formData = new FormData();
+//     formData.append("image", file);
+//     alert("aaa");
+//     const resp = await fetch("https://mnist-api-74qj.onrender.com/predict-frame", {
+//         method: "POST",
+//         body: formData
+//     });
+//     if (!resp.ok) {
+//         throw new Error("ughhh");
+//     }
+//     alert("1");
+//     const data = await resp.json();
+//     alert(data);
+//     document.getElementById("ya").innerText = JSON.stringify(data);
+// }
 async function listenForImageUpload() {
     const inputTag = document.getElementById("camera-input");
     const loadingImage = document.getElementById("processing-image");
